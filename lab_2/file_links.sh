d54f1ffd3436f1c67824b73f036053e4cad3e3d4
sergey <serhg.t@gmail.com>
2019-10-04 23:17:27 +0300

    lab_2 change checks param

--- lab_2/file_links.sh
+++ lab_2/file_links.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ -n "$1" ]
+if [ $# -gt 0 ]
 then
 ls -lt | awk -v file="$1" '/^l/{if ($NF == file) print $(NF-2)}'
 else
--- lab_2/users_in_group.sh
+++ lab_2/users_in_group.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-if [ -n "$1" ]
-then
-awk -v group=$1 -F:  '{if($1 == group) print $NF}' /etc/group | sed 's/,/\n/'
-else
+[ $# -gt 0 ] && 
+awk -v group=$1 -F:  '{if($1 == group) print $NF}' /etc/group | sed 's/,/\n/' || 
 echo "No parameters found. "
-fi
 
